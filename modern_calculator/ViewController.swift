0ec6ab04d7af407a02f81e45bce376a3c2f1d60d
youssef <youssef.mohamed7557@gmail.com>
2019-08-28 19:10:50 +0200

    Initial Commit

---
+++ modern_calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  modern_calculator
+//
+//  Created by Yossef on 8/28/19.
+//  Copyright © 2019 Yossef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
